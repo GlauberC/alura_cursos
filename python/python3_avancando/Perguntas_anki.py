070e61d2cfdef02cb0ef6712fed6b8479929d134
GlauberC <glaubercarv@gmail.com>
2018-08-30 19:05:32 -0300

    Curso de python, avancando na linguagem

--- python/python3_avancando/Perguntas_anki.py
+++ python/python3_avancando/Perguntas_anki.py
@@ -45,3 +45,51 @@ str = str.strip()
 lista = [1, 4, 7, 8]
 print(min(lista))
 print(max(lista))
+
+
+# Crie um tupla com os meses do ano
+# Caso quisermos transformar ela em um calendário de trabalho o qual não possui o mês de dezembro e nem janeiro
+# Como poderia ser feito?
+
+meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto',
+         'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+meses_trabalho = list(meses)
+meses_trabalho.remove('Janeiro')
+meses_trabalho.remove('Dezembro')
+
+
+
+#Para criar um jogo da forca é preciso colocar o caractere underscore para cada letra da palavra
+# como fazer isso rapidamente usando python? (use List Comprehension)
+palavra = 'Teste'
+palavra_secreta = ['_' for letra in palavra]
+
+
+
+#Simplifique o codigo abaixo usando List Comprehension
+
+inteiros = [1,3,4,5,7,8,9]
+pares = []
+for numero in inteiros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+
+inteiros = [1,3,4,5,7,8,9]
+pares = [x for x in inteiros if x % 2 == 0]
+
+
+
+#Ao executar o comando a baixo
+arquivo = open('frutas.txt', 'r')
+for linha in arquivo:
+    print(linha)
+arquivo.close()
+# O usuário obteve como resposta nome de frutas espaçadas por 2 quebras de linhas
+# Isso aconteceu pois o cada linha finaliza com \n e a função print já pula uma linha
+# Como ajeitar isso de duas formas?
+
+print(linha.strip())
+# ou
+print(linha,end='')
+
--- python/python3_avancando/forca.py
+++ python/python3_avancando/forca.py
@@ -5,39 +5,108 @@ import random
 def jogar():
     'Inicia um jogo de forca'
     # Começo de jogo
+    apresentacao()
+
+
+    #Setup
+    palavra_secreta = seleciona_palavra()
+
+    enforcou = False
+    acertou = False
+    erros_maximos = 6
+    erros = 0
+    letras_chutadas = []
+
+    letras_acertadas = esconde_palavra(palavra_secreta)
+
+
+    #Game loop
+    while(not enforcou and not acertou):
+        print(' '.join(letras_acertadas), end='\n\n')
+        chute = pede_um_chute(letras_chutadas)
+
+
+        if(chute in letras_chutadas):
+            print('Essa letra já foi chutada, tente outra\n')
+        else:
+            letras_chutadas.append(chute)
+            if (chute not in palavra_secreta):
+                erros += 1
+                errou_letra(erros)
+
+                #Perdeu
+                if erros == erros_maximos:
+                    print('Você perdeu\n')
+                    enforcou = True
+            else:
+                revela_letras(palavra_secreta, chute, letras_acertadas)
+
+                #Ganhou
+                if '_' not in letras_acertadas:
+                    print('Você acertou a palavra\n')
+                    acertou = True
+
+
+
+    # Fim de jogo
+    encerramento()
+
+def pede_um_chute(letras_chutadas):
+    if len(letras_chutadas) != 0:
+        print('Você já chutou essas letras:\n{}'.format(letras_chutadas))
+    chute = input("Qual a letra? ")
+    return chute.strip().lower()
+
+def apresentacao():
     print(44*'*')
     print('******** Bem vindo ao jogo da forca ********')
     print(44*'*', '\n')
 
-    #Setup
-
-    palavra_secreta = "banana"
-    enforcou = False
-    acertou = False
-
-    letras_acertadas = (((len(palavra_secreta)-1) * '_,') + '_').split(',')
-
-    #Game loop
-    while(not enforcou and not acertou):
-        chute = input("Qual a letra? ")
-        # Tratamento de entrada
-        chute = chute.strip().lower()
-
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra.lower()):
-                letras_acertadas[index] = letra
-            index += 1
+def desenhaForca(num_erros):
+    print(9*"-" + '¬')
+    print(9 * " " + '!')
+    if num_erros >= 1:
+        print(9 * ' ' + 'O')
+    if num_erros == 2:
+        print(9 * ' ' + '|')
+    elif num_erros == 3:
+        print(8 * ' ' + '/' + '|')
+    elif num_erros >=4:
+        print(8 * ' ' + '/' + '|' + '\\')
+    if num_erros == 5:
+        print(8 * ' ' + '/')
+    elif num_erros == 6:
+        print(8 * ' ' + '/' + ' ' + '\\')
+    print('\n\n')
 
 
-        print(' '.join(letras_acertadas),end='\n\n')
+def seleciona_palavra():
+    lista = []
+    arquivo = open('palavras.txt', 'r')
 
-        if '_' not in letras_acertadas:
-            print('Você acertou a palavra\nFim de jogo')
-            acertou = True
+    for palavra in arquivo:
+        lista.append(palavra.strip())
+
+    arquivo.close()
+    palavra_selecionada = lista[random.randrange(0,len(lista))]
+
+    return palavra_selecionada.lower()
 
 
-    # Fim de jogo
+def esconde_palavra(palavra_secreta):
+    return ['_' for letra in palavra_secreta]
+def errou_letra(erros):
+    print('Essa letra não está na palavra')
+    desenhaForca(erros)
+
+def revela_letras(palavra_secreta, chute, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+def encerramento():
     print('\n')
     print(19 * '*')
     print('*** Fim do jogo ***')
