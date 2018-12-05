1ecb2d3dd3d9f01a4555e89a78ec296ae5bbb028
GlauberC <glaubercarv@gmail.com>
2018-12-05 16:36:13 -0300

    Curso de Javascript

---
+++ front_end/Javascript/js/remover-paciente.js
@@ -0,0 +1,13 @@
+var tabela = document.querySelector("#tabela-pacientes")
+
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;              // td
+    var paiAlvoEvento = alvoEvento.parentNode;  // tr
+    
+    paiAlvoEvento.classList.add("fadeOut");
+    setTimeout(function(){
+        paiAlvoEvento.remove();
+    }, 500);
+    
+
+});
