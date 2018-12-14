542c3b05665c679669ee2c39bb4baaca840eb18c
GlauberC <glaubercarv@gmail.com>
2018-12-14 09:56:25 -0300

    Final do curso de ecmascript

--- front_end/Javascript_avancado_I/client/js/app/controllers/NegociacaoController.js
+++ front_end/Javascript_avancado_I/client/js/app/controllers/NegociacaoController.js
@@ -8,13 +8,19 @@ class NegociacaoController{
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($("#mensagemView"));
+        this._mensagemView.update(this._mensagem);
+
     }
     adiciona(event){
         event.preventDefault();
 
-        //  Adicionar negociacao numa lista
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
     _criaNegociacao(){
--- front_end/Javascript_avancado_I/client/js/app/views/NegociacoesView.js
+++ front_end/Javascript_avancado_I/client/js/app/views/NegociacoesView.js
@@ -1,8 +1,8 @@
-class NegociacoesView{
+class NegociacoesView extends View{
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
-    _template(model){
+    template(model){
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -37,7 +37,5 @@ class NegociacoesView{
         </table>
         `
     }
-    update(model){
-        this._elemento.innerHTML = this._template(model);
-    }
+
 }
