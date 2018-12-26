067d2406d2c99e5f62fe87f40bf8619e40a1578f
GlauberC <glaubercarv@gmail.com>
2018-12-26 12:22:18 -0300

    Final do curso de Node

--- back_end/nodejs_I/livraria/src/app/rotas/rotas.js
+++ back_end/nodejs_I/livraria/src/app/rotas/rotas.js
@@ -1,35 +1,77 @@
-module.exports = app => {
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
 
-    app.get('/', (req, resp) => {
-        resp.send(`
-            <html>
-                <head>
-                    <meta charset = "utf-8">
-                    <title>Servidor Node</title>
-                </head>
-                <body>
-                    <h1>Bem vindo ao servidor</h1>
-                </body>
-            </html>`
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.send(
+            `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body>
+                </html>
+            `
         );
     });
     
-    app.get('/livros', (req, resp) => {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
+    app.get('/livros', function(req, resp) {
+
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
                     {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node Avançado'
+                        livros: livros
                     }
-                ]
-            }
-        );
+                ))
+                .catch(erro => console.log(erro));
     });
-    
-}
+
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+
+        livroDao.buscaPorId(id)
+                .then(livro => 
+                    resp.marko(
+                        require('../views/livros/form/form.marko'), 
+                        { livro: livro }
+                    )
+                )
+                .catch(erro => console.log(erro));
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    });
+};
--- back_end/nodejs_I/livraria/src/app/views/livros/form/form.marko.js
+++ back_end/nodejs_I/livraria/src/app/views/livros/form/form.marko.js
@@ -2,53 +2,44 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/casadocodigo$1.0.0/src/app/views/livros/lista/lista.marko",
+    marko_componentType = "/casadocodigo$1.0.0/src/app/views/livros/form/form.marko",
     components_helpers = require("marko/src/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
     marko_helpers = require("marko/src/runtime/html/helpers"),
     marko_loadTag = marko_helpers.t,
     component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
-    marko_forEach = marko_helpers.f,
-    marko_escapeXml = marko_helpers.x,
     marko_escapeXmlAttr = marko_helpers.xa,
+    marko_escapeXml = marko_helpers.x,
     init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
 
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<html><head><meta charset=\"utf-8\"></head><body>");
+  out.w("<html><body>");
 
   component_globals_tag({}, out);
 
-  out.w("<h1> Listagem de livros </h1><table id=\"livros\"><tr><td>ID</td><td>Título</td><td>Preço</td><td>Editar</td><td>Remover</td></tr>");
+  out.w("<h1>Cadastro de livros</h1><form action=\"/livros\" method=\"post\">");
 
-  var for__12 = 0;
+  if (data.livro.id) {
+    out.w("<div><input type=\"hidden\" name=\"_method\" value=\"PUT\"><input type=\"hidden\" id=\"id\" name=\"id\" value=\"" +
+      marko_escapeXmlAttr(data.livro.id) +
+      "\"></div>");
+  }
 
-  marko_forEach(data.livros, function(livro) {
-    var keyscope__13 = "[" + ((for__12++) + "]");
-
-    out.w("<tr id=\"livro_" +
-      marko_escapeXmlAttr(livro.id) +
-      "\"><td>" +
-      marko_escapeXml(livro.id) +
-      "</td><td>" +
-      marko_escapeXml(livro.titulo) +
-      "</td><td>" +
-      marko_escapeXml(livro.preco) +
-      "</td><td><a href=\"/livros/form/" +
-      marko_escapeXmlAttr(livro.id) +
-      "\">Editar</a></td><td><a href=\"#\" data-ref=\"" +
-      marko_escapeXmlAttr(livro.id) +
-      "\" data-type=\"remocao\">Remover</a></td></tr>");
-  });
-
-  out.w("</table><script src=\"/estatico/js/remove-livro.js\">\r\n        </script>");
+  out.w("<div><label for=\"titulo\">Titulo:</label><input type=\"text\" id=\"titulo\" name=\"titulo\" value=\"" +
+    marko_escapeXmlAttr(data.livro.titulo) +
+    "\" placeholder=\"coloque o titulo\"></div><div><label for=\"preco\">Preço:</label><input type=\"text\" id=\"preco\" name=\"preco\" value=\"" +
+    marko_escapeXmlAttr(data.livro.preco) +
+    "\" placeholder=\"150.25\"></div><div><label for=\"descricao\">Descrição:</label><textarea cols=\"20\" rows=\"10\" id=\"descricao\" name=\"descricao\" placeholder=\"fale sobre o livro\">" +
+    marko_escapeXml(data.livro.descricao) +
+    "</textarea></div><input type=\"submit\" value=\"Salvar\"></form>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "23");
+  await_reorderer_tag({}, out, __component, "17");
 
   out.w("</body></html>");
 }
@@ -61,7 +52,7 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/casadocodigo$1.0.0/src/app/views/livros/lista/lista.marko",
+    id: "/casadocodigo$1.0.0/src/app/views/livros/form/form.marko",
     tags: [
       "marko/src/components/taglib/component-globals-tag",
       "marko/src/components/taglib/init-components-tag",
